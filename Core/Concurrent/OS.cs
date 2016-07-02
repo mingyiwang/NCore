b39cd130f95ae57ae64cbf092d89cfca8090565b
Frank <mingyi.frank@gmail.com>
2016-07-02 15:41:24 +1000

    Updates

--- Core/Concurrent/OS.cs
+++ Core/Concurrent/OS.cs
@@ -2,14 +2,16 @@
 
 namespace Core.Concurrent
 {
+
     public static class OS
     {
-        public static readonly int  BitsPerByte = 8;
-        public static readonly int  CpuCount = Environment.ProcessorCount;
-        public static readonly int  CorePoolSize = CpuCount + 1;
-        public static readonly int  MinPoolSize = 1;
+
+        public static readonly int  BitsPerByte     = 8;
+        public static readonly int  CpuCount        = Environment.ProcessorCount;
+        public static readonly int  CorePoolSize    = CpuCount + 1;
+        public static readonly int  MinPoolSize     = 1;
         public static readonly int  MaximumPoolSize = CpuCount * 2 + 1;
-        public static readonly bool IsLittleEndian = BitConverter.IsLittleEndian;
+        public static readonly bool IsLittleEndian  = BitConverter.IsLittleEndian;
 
     }
 
